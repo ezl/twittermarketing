fe4216add2dc7c92ccca8ffb8d7e4770571008fa
E <eric@ubuntupc.(none)>
2010-07-16 13:27:39 -0500

    clean code for readability

--- main/management/commands/get_twitter_users.py
+++ main/management/commands/get_twitter_users.py
@@ -38,8 +38,8 @@ class Command(NoArgsCommand):
     def contact_new_followers(self):
         # check for new followers
         print "[Check for new followers]"
-        my_followers_ids = api.followers_ids(self.me.id)
-        new_followers = TwitterUser.objects.filter(twitter_id__in=my_followers_ids,
+        followers_ids = api.followers_ids(self.me.id)
+        new_followers = TwitterUser.objects.filter(twitter_id__in=followers_ids,
                                                      following_me=False)
         print "  - New followers: %s" % new_followers.count()
         # send any new followers a message
@@ -49,22 +49,25 @@ class Command(NoArgsCommand):
             new_follower.save()
             if random.randint(1, 16) == 11:
                 status_message = random.sample(status_messages, 1)[0]
-                api.update_status("@%s %s" (new_follower.screen_name, status_message))
-                print "    - Updated status, message to: %s" % new_follower.screen_name
+                api.update_status("@%s %s" (new_follower.screen_name,
+                                            status_message))
+                print "    - Updated status to: %s" % new_follower.screen_name
             else:
                 direct_message = random.sample(direct_messages, 1)[0]
-                api.send_direct_message(user_id=new_follower.twitter_id, text=direct_message)
+                api.send_direct_message(user_id=new_follower.twitter_id,
+                                        text=direct_message)
                 print "    - Sent DM to: %s" % new_follower.screen_name
 
     def prune_losers(self):
         print "[Prune losers]"
-        reciprocation_window = 24 * 3 # hours
         # check to see if people i followed follow me back
+        reciprocation_window = 24 * 3 # hours
+        cutoff_time = datetime.now() - timedelta(hours=reciprocation_window)
         unchecked = TwitterUser.objects.filter(checked_for_reciprocation=False,
-                                               followed_on__lt=datetime.now() - timedelta(hours=reciprocation_window))
+                                               followed_on__lt=cutoff_time)
         print "  - Checking %s users for recent follow" % unchecked.count()
         losers = unchecked.filter(following_me=False)
-        print "  - Reciprocated follow: %s" % (unchecked.count() - losers.count())
+        print "  - Reciprocated follow: %s" % (unchecked.count()-losers.count())
         print "  - Losers: %s" % losers.count()
         for loser in losers:
             loser.checked_for_reciprocation = True
@@ -72,54 +75,57 @@ class Command(NoArgsCommand):
             try:
                 api.destroy_friendship(loser)
             except TweepError, e:
-                print "                                              TweepError: %s: %s" % (loser.screen_name, e)
+                print "TweepError: %s: %s" % (loser.screen_name, e)
 
     def find_new_followers(self):
         print "[Find new followers]"
-        queries = ["$GOOG", "$GOOG", "$GOOG", "$GOOG", "$GOOG", "$GOOG", "$GOOG", "$GOOG", "$GOOG", 
-                   "$AAPL", "$AAPL", "$AAPL", "$AAPL", "$AAPL", "$AAPL", "$AAPL", "$AAPL", "$AAPL", "$AAPL", 
-                   "$MSFT", "$TSLA", "$AOL", "$GS", "$HPQ",]
-        q = random.sample(queries, 1)  # query to filter for
-        n = 500     # number of statuses to retrieve
-        statuses = [c for c in Cursor(api.search, q=q, lang="en").items(n)]
+        n = 20     # number of statuses to retrieve per query
+        search_dict = dict()
+        search_dict['lang'] = "en"
+        # search_dict['geocode'] = "41.877630,-87.624389,35mi" # chicago
+
+        statuses = list()
+        queries = ["$GOOG", "$AAPL", "$TSLA", "$MSFT", "$GS", "$MS",
+                   "Berkshire Hathaway", "$HQ", "$INTC", "$CSCO"]
+            print "  - query:"
+        for q in queries:
+            search_dict['q'] = q
+            results = [c for c in Cursor(api.search, **search_dict).items(n)]
+            print "    - %s: %s hits" % (q, len(results)),
+            statuses.extend(results)
+        print
 
         # find the likely human candidates
         # people talking to one another. get recipients.
         twitter_usernames = [s.to_user for s in statuses if s.to_user_id]
-        # bullshit. SearchResult.to_user_id doesn't map to the right user id.
-        # http://stackoverflow.com/questions/3256981/tweepy-api-how-to-get-a-users-id-from-a-status-searchresult-object
 
         # from a likely to be "normal human" source
         twitter_usernames.extend([s.from_user for s in
                                   filter(likely_human, statuses)])
-        twitter_usernames = set(twitter_usernames)
+        twitter_usernames = list(set(twitter_usernames))
+
+        # TODO: store these into a list of followers to follow next time
+        # arbitrarily chose 120
+        if len(twitter_usernames) > 120:
+            twitter_usernames = twitter_usernames[:120]
+
         twitter_users = [free_api.get_user(u) for u in twitter_usernames]
         twitter_ids = [u.id for u in twitter_users]
-        print "  - query: %s" % q
-        print "  - probable human: %s / %s " % (len(twitter_ids), n)
 
-        # TODO: filter by location order.  this is sloppy.
-
-        # filter by location
-        filter_by_location = None
-        chi_geocode = "41.877630,-87.624389,35mi"
-        if filter_by_location is not None:
-            print "filtering by location"
-
-            local_users = [u for u in twitter_users \
-                           if filter_by_location.lower() in u.location.lower()]
+        print "  - likely human: %s/%s " % (len(twitter_ids), n * len(queries))
 
         # only follow new people
         previously_followed = [i.twitter_id for i in
                   TwitterUser.objects.filter(twitter_id__in=twitter_ids)]
-        new_twitter_ids = filter(lambda x: not x in previously_followed, twitter_ids)
+        new_twitter_ids = filter(lambda x: not x in previously_followed,
+                                 twitter_ids)
         print "  - new humans: %s" % len(new_twitter_ids)
         newly_followed = 0
         for twitter_id in new_twitter_ids:
             try:
                 api.create_friendship(twitter_id)
             except TweepError, e:
-                print "                                    Skipping. TweepError: %s: %s" % (twitter_id, e)
+                print "Skipping id %s. TweepError: %s" % (twitter_id, e)
             else:
                 t = TwitterUser(twitter_id=twitter_id)
                 try:
